94f2bf00b3573b638a72b4f9798d94c42f7d2c06
SubrotoChandaShuvo <140307494+SubrotoChandaShuvo@users.noreply.github.com>
2023-10-17 12:29:25 +0600

    Minimum_coin_change.cpp

---
+++ Minimum_coin_change.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int co;
+    cout<<"Enter the number of coin : ";
+    cin>>co;
+    int tk;
+    cout<<"Enter the Amount of TK : ";
+    cin>>tk;
+    int arr[co];
+    for(int i=0; i<co; i++)
+    {
+        cout<<"Coin number "<<i+1<<" : ";
+        cin>>arr[i];
+    }
+
+    int table[co+1][tk+1];
+
+     for (int i = 1; i <= co; i++)
+    {
+        table[i][0] =0;
+    }
+    for (int i = 0; i <= tk; i++)
+    {
+        table[0][i] =tk+11;
+    }
+   
+
+    for(int i=1; i<=co; i++)
+    {
+        for(int j=1; j<=tk; j++)
+        {
+            if(arr[i-1]>j)
+            table[i][j]=table[i-1][j];
+            else
+            {
+               table[i][j]=min(table[i][j-arr[i-1]]+1,table[i-1][j]);
+            }
+        }
+      
+    }
+     cout<<endl<<"THE MINIMUM COIN CHANGE : "<<table[co][tk]<<endl;
+     for(int i=0; i<=co; i++)
+    {
+        for(int j=0; j<=tk; j++)
+        {
+            cout<<table[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
