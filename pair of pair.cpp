ccdc2a9dc8d053ae0825f32240ea932efe83a3d4
SubrotoChandaShuvo <140307494+SubrotoChandaShuvo@users.noreply.github.com>
2023-11-21 05:33:31 +0600

    pair of pair.cpp

---
+++ pair of pair.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int, pair<int,int>>p;
+    cin>>p.first;
+    cin>>p.second.first;
+    cin>>p.second.second;
+    cout<<p.first<<" "<<p.second.first<<" "<<p.second.second;
+    return 0;
+}
